0b7f57f160625c0812ad79b337d802f35f3d38db
Nicolas-Barraza-Estrada <nicolas.barraza1901@alumnos.ubiobio.cl>
2023-10-24 12:21:44 -0300

    primer commit

---
+++ src/config/initialSetup.js
@@ -0,0 +1,81 @@
+"use strict";
+// Importa el modelo de datos 'Role'
+const Role = require("../models/role.model.js");
+const User = require("../models/user.model.js");
+
+/**
+ * Crea los roles por defecto en la base de datos.
+ * @async
+ * @function createRoles
+ * @returns {Promise<void>}
+ */
+async function createRoles() {
+  try {
+    // Busca todos los roles en la base de datos
+    const count = await Role.estimatedDocumentCount();
+    // Si no hay roles en la base de datos los crea
+    if (count > 0) return;
+
+    await Promise.all([
+      new Role({ name: "Postulante" }).save(),
+      new Role({ name: "admin" }).save(),
+      new Role({name: "Examinador"}).save(),
+      new Role({name: "Secretaria"}).save()
+    ]);
+    console.log("* => Roles creados exitosamente");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * Crea los usuarios por defecto en la base de datos.
+ * @async
+ * @function createUsers
+ * @returns {Promise<void>}
+ */
+async function createUsers() {
+  try {
+    const count = await User.estimatedDocumentCount();
+    if (count > 0) return;
+
+    const admin = await Role.findOne({ name: "admin" });
+    const user = await Role.findOne({ name: "Postulante" });
+    const exam = await Role.findOne({name: "Examinador"})
+
+    await Promise.all([
+      new User({
+        username: "user",
+        email: "user@dominio.cl",
+        password: await User.encryptPassword("user123"),
+        roles: user._id,
+      }).save(),
+      new User({
+        username: "admin",
+        email: "admin@dominio.cl",
+        password: await User.encryptPassword("admin123"),
+        roles: admin._id,
+      }).save(),
+      new User({
+        username:"Examinador",
+        email: "exam@dominio.cl",
+        password: await User.encryptPassword("exam123"),
+        roles: exam._id,
+      }).save(),
+      new User({
+        username:"Secretaria",
+        email: "sec@dominio.cl",
+        password: await User.encryptPassword("sec123"),
+        roles: exam._id,
+      }).save()
+    ]);
+    console.log("* => Users creados exitosamente");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+module.exports = {
+  createRoles,
+  createUsers,
+};
---
+++ src/constants/roles.constants.js
@@ -0,0 +1,4 @@
+/** Roles permitidos por la base de datos */
+const ROLES = ["Postulante","admin", "Examinador","Secretaria"];
+
+module.exports = ROLES;
